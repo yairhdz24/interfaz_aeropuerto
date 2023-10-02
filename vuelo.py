f1f02c080b0911a40b0d482a06703e285fe3df41
Yair Hdz <yairhdz107@gmail.com>
2023-10-01 20:15:04 -0600

    logica de tabla añadida}

--- vuelo.py
+++ vuelo.py
@@ -1,18 +1,47 @@
+
 class Vuelo:
-    def __init__(self, id="", origen="", destino="", peso=0):
+    def __init__(self, id = "", origen = "", destino = "", peso = 0):
         self.__id = id
         self.__origen = origen
         self.__destino = destino
         self.__peso = peso
-        
-        
+    
     def __str__(self):
         return(
-            "Identificador: " + str(self.__id) + "\n" +
-            "Origen: " + self.__origen +    "\n" +
-            "Destino: " + self.__destino +  "\n" +
-            "Peso: " + str(self.__peso) +   "\n" 
+            "Identificador: " + self.__id + "\n" +
+            "Origen: " + self.__origen + "\n" +
+            "Destino: " + self.__destino + "\n" +
+            "Peso: " + str(self.__peso) + "\n" 
         )
-        
-# v01 = Vuelo(id="0", origen="GDL", destino="MTY", peso=20)
-# print(v01)
+
+    def to_dict(self):
+        return{
+            "id": self.__id,
+            "origen":self.__origen,
+            "destino":self.__destino,
+            "peso":self.__peso
+        }
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def origen(self):
+        return self.__origen
+    
+    @property
+    def destino(self):
+        return self.__destino
+    
+    @property
+    def peso(self):
+        return self.__peso
+    
+    @property
+    def id(self):
+        return self.__id
+    
+
+#v01= Vuelo(id ="0", origen="a", destino= "b", peso = 2)
+#help(v01)
